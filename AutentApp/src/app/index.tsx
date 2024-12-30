56b86f84cc62f724b43707b12bd1c67c3f00b3bf
Geeh17 <geraldoluis1913@hotmail.com>
2024-12-29 21:50:48 -0300

    Criação inicial da parte estrutural de pastas do App

---
+++ AutentApp/src/app/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text, StyleSheet } from "react-native";
+
+export default function app() {
+  return (
+    <View>
+      <Text>Login</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
